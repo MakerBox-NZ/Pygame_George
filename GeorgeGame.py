d254971f55bf4493b3daa7b377054129a44e2fdf
Coinmat3r <twenty1gaming@gmail.com>
2017-06-21 16:52:36 +1200

    added moving code for keys

--- GeorgeGame.py
+++ GeorgeGame.py
@@ -8,6 +8,8 @@ class Player(pygame.sprite.Sprite):
      #Spawn a player
      def __init__(self):
           pygame.sprite.Sprite.__init__(self)
+          self.momentumX = 0 #move along X
+          self.momentumY = 0 #move along Y
           self.images = [  ]
           img = pygame.image.load(os.path.join('images','hero.png')).convert()
           self.images.append(img)
@@ -16,8 +18,26 @@ class Player(pygame.sprite.Sprite):
 
           self.image.convert_alpha() #optimise for alpha
           self.image.set_colorkey(alpha) #set alpha
+     def control (self, x, y):
+          #control player movement
+          self.momentumX += x
+          self.momentumY += y
+
+     def update(self):
+          #update sprite position
+          currentX = self.rect.x
+          nextX = currentX + self.momentumX
+          self.rect.x = nextX
+
+          currentY = self.rect.y
+          nextY = currentY + self.momentumY
+          self.rect.y = nextY
+
+               
+
           
-          
+
+               
          
           
           
@@ -50,33 +70,39 @@ player.rect.x = 0
 player.rect.y = 0
 movingsprites = pygame.sprite.Group()
 movingsprites.add(player)
+movesteps = 10 #how fast to move
 
 '''MAIN LOOP'''
 # code runs many times
 while main == True:
     for event in pygame.event.get():
-        if  event.type == pygame.KEYUP:
+        if  event.type == pygame.KEYDOWN:
           if event.key == ord('q'):
                 pygame.quit()
                 sys.exit()
                 main = False
           if event.key == ord('a'):
+               player.control(-movesteps, 0)
                print('left stop')
           if event.key == ord('d'):
+               player.control(movesteps, 0)
                print('right stop')
           if event.key == ord('w'):
                print('up stop')
 
-
-        if event.type == pygame.KEYDOWN:
-          if event.key == ord('a'):
+        if event.type == pygame.KEYUP:
+         if event.key == ord('a'):
+               player.control(movesteps, 0)
                print('left')
-          if event.key == ord('d'):
+         if event.key == ord('d'):
+               player.control(-movesteps, 0)
                print('right')
-          if event.key == ord('w'):
+         if event.key == ord('w'):
                print('up')
 
+
     screen.blit(backdrop, backdropRect)
+    player.update() #update player postion
     movingsprites.draw(screen)  #draw player
 
     pygame.display.flip()
